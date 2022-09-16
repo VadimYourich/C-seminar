4e7a22e32bc078e6cd7fd1169606b1378302c176
vadim <89281779909@mail.ru>
2022-09-16 19:31:14 +0300

    домашнее задание 4

---
+++ Seminar4/HW4/task27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSum(int num)
+{
+    int sum = 0;
+    int count = 0;
+    if (num == 0) sum = 0;
+    while(num != 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+        count++;
+    }
+    return sum;
+}
+
+int number = ReadNumber("Введите число:");
+int sum = GetSum(number);
+Console.WriteLine(sum);
---
+++ Seminar4/HW4/task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// Данные вводятся с консоли пользователем
+Console.WriteLine("Введите длину массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+for (int n = 0; n < m; n++)
+{
+    Console.WriteLine("Введите элементы массива");
+    array[n] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Введённый массив: ");
+for (int n = 0; n < m; n++)
+{
+    Console.Write(array[n]);
+}
