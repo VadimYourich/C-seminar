d42fff1117a9a9ed571f39fa31b838833c43cc7f
vadim <89281779909@mail.ru>
2022-10-09 19:15:29 +0300

    HW 9

---
+++ Seminar9/HW9/task64/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNTo1(int n)
+{
+    if (n <= 0)
+    {
+        return;
+    }
+    else
+    {
+        Console.Write(n + " ");
+        PrintNTo1(n - 1);
+    }
+}
+
+int n = ReadNumber("Введите положительное число N: ");
+if (n < 0)
+{
+    Console.WriteLine("Нужно положительное число!");
+}
+else PrintNTo1(n);
+
---
+++ Seminar9/HW9/task68/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n += 1;
+    else if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    else if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    return Akkerman(m, n);
+}
+
+int m = ReadNumber("Введите неотрицательное число M: ");
+int n = ReadNumber("Введите неотрицательное число N: ");
+
+if (m < 0 || n < 0)
+{
+    Console.WriteLine("Нужны неотрицательные числа!");
+}
+else Console.WriteLine(Akkerman(m, n));
